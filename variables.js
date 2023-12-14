757d8e2e6126c2623a5512c3a2b469fd65c68d9b
Shridhar Bhat <bhatshridhar27@gmail.com>
2023-12-14 15:21:55 +0530

    variables & datatypes

---
+++ variables.js
@@ -0,0 +1,82 @@
+// varaible keywords 
+
+// var keyword
+var x; // declaration
+var x; // re-declaration
+var x=10; // initialization
+x=25; // re-initialization
+console.log(x);
+
+// let keyword
+let y;
+y=10;
+y=30;
+console.log(y);
+
+// const keywords
+
+const PI=3.142; // declaration & initialization in one line
+console.log(PI);
+
+// datatypes
+
+// primitive datatypes
+
+let num=10;
+console.log(typeof(num));
+console.log(num);
+
+console.log("-----------------------------------------");
+
+let text="hello"
+let text1='hii'
+let text2=`India`
+console.log(typeof(text2));
+console.log(text2);
+
+console.log("---------------------------------------------");
+
+let num2=5555555555555555222222222222n;
+console.log(typeof(num2));
+console.log(num2);
+
+console.log("------------------------------------------------");
+
+let data;
+console.log(data);
+console.log(typeof(data));
+
+console.log("----------------------------------------------------");
+
+let data2=null;
+console.log(data2);
+console.log(typeof data2);
+
+console.log("--------------------------------------------");
+
+let bool=true;
+console.log(bool);
+console.log(typeof bool);
+
+console.log("--------------------------------------------------");
+
+// non-primitive datatypes
+
+let arr=[1,2,5.6,'true']
+console.log(arr);
+console.log(typeof arr);
+
+function greet(){
+    console.log("Hello Everyone");
+}
+greet()
+
+let msg={
+    "id":1,
+    "name":"update"
+}
+console.table(msg);
+console.log(typeof(msg));
+
+
+
