68bc7cdce83af370b1f64ab3ced1b23668e32fe0
Shridhar Bhat <bhatshridhar27@gmail.com>
2023-12-14 15:45:06 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,72 @@
+// operators 
+
+// Arithmetic operators
+let a = 10;
+let b = 5;
+
+console.log(a + b); // Addition: 15
+console.log(a - b); // Subtraction: 5
+console.log(a * b); // Multiplication: 50
+console.log(a / b); // Division: 2
+console.log(a % b); // Modulus (Remainder): 0
+console.log(a ** b); // Exponentiation: 100000
+
+// relational operators
+
+console.log("-------------------------------------------------");
+let x=10;
+let y=5;
+console.log(x > y); // Greater than: true
+console.log(x < y); // Less than: false
+console.log(x >= y); // Greater than or equal to: true
+console.log(x <= y); // Less than or equal to: false
+console.log(x === y); // comapare both datatypes and values 
+console.log(x == y); // compare only values
+
+console.log("---------------------------------------");
+
+// ternary operators
+let age = 20;
+let allowed = (age > 18) ? 'Yes' : 'No';
+console.log(allowed); // Output: 'Yes'
+
+console.log("------------------------------------------");
+
+// logical operators
+let p = true;
+let q = false;
+
+console.log(p && q); // Logical AND: false
+console.log(p || q); // Logical OR: true
+console.log(!p); // Logical NOT: false
+
+console.log("------------------------------------");
+
+// assignment operators
+let y1 = 5;
+y1 += 3; // Equivalent to y1 = y1 + 3
+console.log(y1); // Output: 8
+
+let z = 10;
+z -= 4; // Equivalent to z = z - 4
+console.log(z); // Output: 6
+
+let a1 = 3;
+a1 *= 5; // Equivalent to a1 = a1 * 5
+console.log(a1); // Output: 15
+
+let b2 = 20;
+b2 /= 4; // Equivalent to b2 = b2 / 4
+console.log(b2); // Output: 5
+
+let c = 17;
+c %= 5; // Equivalent to c = c % 5
+console.log(c); // Output: 2
+
+
+
+
+
+
+
+
